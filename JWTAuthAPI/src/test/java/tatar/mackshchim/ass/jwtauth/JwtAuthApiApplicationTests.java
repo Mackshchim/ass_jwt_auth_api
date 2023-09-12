55ed881a2e8210a66af8399a833e3a1686c40360
Mackshchim <mackshchim@gmail.com>
2023-09-12 15:31:14 +0300

    project spring initialization

---
+++ JWTAuthAPI/src/test/java/tatar/mackshchim/ass/jwtauth/JwtAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tatar.mackshchim.ass.jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
