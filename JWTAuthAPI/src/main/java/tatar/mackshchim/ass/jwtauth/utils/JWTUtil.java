2d3bca44cad64c43c3aae8838b9e950b1d02c17a
Mackshchim <mackshchim@gmail.com>
2023-09-13 19:08:24 +0300

    implimented JWTUtil

---
+++ JWTAuthAPI/src/main/java/tatar/mackshchim/ass/jwtauth/utils/JWTUtil.java
@@ -0,0 +1,10 @@
+package tatar.mackshchim.ass.jwtauth.utils;
+
+import tatar.mackshchim.ass.jwtauth.DTO.AccessRefreshTokensDTO;
+import tatar.mackshchim.ass.jwtauth.models.User;
+
+public interface JWTUtil {
+
+    AccessRefreshTokensDTO buildAccessRefreshTokens(User user);
+
+}
